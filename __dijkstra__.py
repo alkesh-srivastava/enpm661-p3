288729d806b6cff501d3d1e03c65d59fb5aebf2a
alkesh-umd <78883501+alkesh-umd@users.noreply.github.com>
2021-03-26 11:19:22 -0400

    Update __dijkstra__.py

--- __dijkstra__.py
+++ __dijkstra__.py
@@ -29,3 +29,30 @@ def dijkstra(maze, start, goal):
                 pq.put(neighbour, f_value)
                 predecessors[neighbour] = current_cell
     return None
+
+
+def explored_path(maze, start, goal):
+    pq = PriorityQueue()
+    pq.put(start, 0)
+    predecessors = {start: None}
+    g_values = {start: 0}
+    coordinates = list()
+    while not pq.is_empty():
+        current_cell = pq.get()
+        if current_cell == goal:
+            return coordinates
+
+        for direction in ["up", "right", "down", "left", "up-left", "up-right", "down-left", "down-right"]:
+            row_offset, col_offset = offsets[direction]
+            neighbour = (current_cell[0] + row_offset, current_cell[1] + col_offset)
+            if move_possible(maze, neighbour) and neighbour not in g_values:
+                if direction == "up" or direction == "right" or direction == "down" or direction == "left":
+                    new_cost = g_values[current_cell] + 1
+                else:
+                    new_cost = g_values[current_cell] + math.sqrt(2)
+                g_values[neighbour] = new_cost
+                f_value = new_cost
+                pq.put(neighbour, f_value)
+                predecessors[neighbour] = current_cell
+                coordinates.append(neighbour)
+    return None
